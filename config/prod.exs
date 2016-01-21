68bd18020342c324cfa172f126919ad0fe1092af
Alex Jensen <96dalex@gmail.com>
2016-01-21 13:16:49 -0700

    wrong app

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :elixir_chess, ElixirChess.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "blooming-fjord-12074.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "pure-everglades-88964.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
